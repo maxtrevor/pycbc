7500eb3ea6312b86e9d65ba0b0800f0166846402
Moz-Port <simone.mozzon@port.ac.uk>
2021-03-09 13:47:24 -0800

    Fix condition in cat2.py

--- pycbc/workflow/cat2.py
+++ pycbc/workflow/cat2.py
@@ -8,8 +8,9 @@ class PyCBCCalculateDqExecutable(Executable):
     # This is a wrapper around the pegasus class "Executable"
     # The arguments of pegasus.Executable can be found in
     # pycbc/workflow/core.py#L149
-    current_retention_level = Executable.ALL_TRIGGERS
-    def create_node(self, cat2_file, flag, ifo, start, end):
+    # current_retention_level = Executable.ALL_TRIGGERS
+    current_retention_level = Executable.MERGED_TRIGGERS
+    def create_node(self, cat2_file, flag, start, end):
         node = Node(self)
         # Here I have to add just the argument related to the workflow.
         # Everything else will have to be in analysis.ini 
@@ -27,7 +28,7 @@ class PyCBCCalculateDqExecutable(Executable):
 
 
 def create_cat2_timeseries(workflow, cat2_file, cat2_name, option_name, output_dir=None, tags=None):   
-    if not option_name in workflow.cp.get_subsections('workflow-segments'):
+    if not workflow.cp.has_option('workflow-coincidence', 'do-dq-fitting'):
         return FileList()
     else:
         start = workflow.analysis_time[0]
@@ -41,13 +42,12 @@ def create_cat2_timeseries(workflow, cat2_file, cat2_name, option_name, output_d
 
             # Probably it will read letter by letter. 
             for flag in flag_str:
-                print(flag_str)
                 raw_exe = PyCBCCalculateDqExecutable(workflow.cp, 
                                                      'calculate_dq', ifos=ifo,
                                                      out_dir=output_dir, 
                                                      tags=tags)
                 # How does it know what name to give to the output file??
-                raw_node = raw_exe.create_node(cat2_files, flag_str, ifo, start, end)
+                raw_node = raw_exe.create_node(cat2_file, flag_str, start, end)
                 workflow += raw_node
                 output += raw_node.output_files
         return output
