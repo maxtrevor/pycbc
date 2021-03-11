cfbf3aa34d666cf227302a0e40727a100b25baa7
Moz-Port <simone.mozzon@port.ac.uk>
2021-03-11 14:59:43 -0800

    Calculate dq timeseries on science segments

--- pycbc/workflow/cat2.py
+++ pycbc/workflow/cat2.py
@@ -3,51 +3,48 @@ import logging
 from ligo import segments
 from pycbc.workflow.core import FileList, Executable, Node, File
 
-
 class PyCBCCalculateDqExecutable(Executable):
     # This is a wrapper around the pegasus class "Executable"
     # The arguments of pegasus.Executable can be found in
     # pycbc/workflow/core.py#L149
     # current_retention_level = Executable.ALL_TRIGGERS
     current_retention_level = Executable.MERGED_TRIGGERS
-    def create_node(self, cat2_file, flag, start, end):
+
+    def create_node(self, workflow, segments, cat2_file, flag):
         node = Node(self)
         # Here I have to add just the argument related to the workflow.
-        # Everything else will have to be in analysis.ini 
+        # Everything else will have to be in analysis.ini
         # [calculate_dq]
-
         # Executable objects are initialized with ifo information
-        segment = segments.segment(start, end)
         node.add_opt('--ifo', self.ifo_string)
-        node.add_input_opt('--cat2-segments', cat2_file)
-        node.add_opt('--gps-start-time', start)
-        node.add_opt('--gps-end-time', end)
         node.add_opt('--flag', flag)
-        node.new_output_file_opt(segment, '.hdf', '--output-file')        
+        node.add_opt('--analyzable-segments', segments)
+        node.add_input_opt('--cat2-segments', cat2_file)
+        node.new_output_file_opt(workflow.analysis_time, '.hdf', '--output-file')
+        # Comment: I can find these files using find_output_with_tag(self, tag)
+        # from core.py
         return node
 
 
-def create_cat2_timeseries(workflow, cat2_file, cat2_name, option_name, output_dir=None, tags=None):   
+def create_cat2_timeseries(workflow, segs, cat2_file, cat2_name, option_name,
+                           output_dir=None, tags=None):
     if not workflow.cp.has_option('workflow-coincidence', 'do-dq-fitting'):
         return FileList()
     else:
-        start = workflow.analysis_time[0]
-        end = workflow.analysis_time[1]
-        
+        if not tags: tags=[]
         output = FileList()
         for ifo in workflow.ifos:
-            flag_str = workflow.cp.get_opt_tags("workflow-segments", option_name, [ifo])
-            # I have to declare the executable in executable.ini with something like 
-            # calculate_dq = pycbc_calculate_dq. I have to do this for each exec.
-
-            # Probably it will read letter by letter. 
+            flag_str = workflow.cp.get_opt_tags("workflow-segments",
+                                                option_name, [ifo])
             for flag in flag_str:
-                raw_exe = PyCBCCalculateDqExecutable(workflow.cp, 
+                flag_tag = tags
+                flag_tag.append(flag_str)
+                raw_exe = PyCBCCalculateDqExecutable(workflow.cp,
                                                      'calculate_dq', ifos=ifo,
-                                                     out_dir=output_dir, 
-                                                     tags=tags)
-                # How does it know what name to give to the output file??
-                raw_node = raw_exe.create_node(cat2_file, flag_str, start, end)
+                                                     out_dir=output_dir,
+                                                     tags=flag_tag)
+                raw_node = raw_exe.create_node(workflow, segs[ifo], cat2_file, 
+                                               flag_str)
                 workflow += raw_node
                 output += raw_node.output_files
-        return output
+    return output
